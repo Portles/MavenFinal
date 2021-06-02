c58fa6d78cfe3930aa92fef54616644b32b0ed9f
Nizamet Özkan <nizamet005@gmail.com>
2021-06-02 15:51:30 +0300

    xdxd

--- YazilimAraclariFinal/src/test/java/tr/yazilimaraclari/h5190001/YazilimAraclariFinal/YazilimAraclariFinalApplicationTests.java
+++ YazilimAraclariFinal/src/test/java/tr/yazilimaraclari/h5190001/YazilimAraclariFinal/YazilimAraclariFinalApplicationTests.java
@@ -1,5 +1,9 @@
 package tr.yazilimaraclari.h5190001.YazilimAraclariFinal;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +12,21 @@ class YazilimAraclariFinalApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+	
+	@Test
+	public final void getProductTest() {
+		List<Product> list = ProductStore.getProducts();
+		Boolean isEmpty = list.isEmpty();
+		assertEquals(false, isEmpty);	
+	}
+	
+	@Test
+	public final void addProductTest() {
+		Product products = new Product("selam", "7");
+		List<Product> list = ProductStore.addProduct(products);
+		Boolean isAdded = list.contains(products);
+		assertEquals(true, isAdded);
 	}
 
 }
